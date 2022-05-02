c835605fdb98a310afa6e7f850370a0a1c26dcf5
Tabu Hana <taboodog@hotmail.com>
2022-05-02 10:27:37 -0700

    fonts

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -21,13 +21,13 @@ const AppNavbar = () => {
           <Navbar.Collapse id='navbar'>
             <Nav className='ml-auto'>
               <Nav.Link as={Link} to='/'>
-                Search For Games
+                [Search For Games] 
               </Nav.Link>
               {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
                 <>
                   <Nav.Link as={Link} to='/saved'>
-                    View Saved Games
+                    [View Saved Games]
                   </Nav.Link>
                   <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                 </>
